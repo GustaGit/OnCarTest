54929d33ff3c7d5c0a2885b9465312487984db1f
Gustavo Sousa <gus.tavosousatec@gmail.com>
2024-02-27 07:51:05 -0300

    delete comments

--- OnCarVManager.Web/src/app/services/finance-simulator/simulation.service.ts
+++ OnCarVManager.Web/src/app/services/finance-simulator/simulation.service.ts
@@ -14,20 +14,6 @@ export class SimulationService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public AddSimulationTest(simulation:Simulation): User{
-
-    //TODO: Serviço funcionar
-    return {
-      documentCPF: simulation.documentCPF,
-      email: simulation.email,
-      phone:simulation.phone,
-      birthdate: simulation.birthdate,
-      familyIncome: simulation.familyIncome,
-      carPrice:50000,
-      score:500
-    }
-  }
-
   public AddSimulation(simulation: Simulation): Observable<User> {
     return this.httpClient.post<User>(`${Constants.API_BASE_URL}/${this.simulationController}`, simulation);
   }
